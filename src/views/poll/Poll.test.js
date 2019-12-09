ca98d73713351b306ac030609647713a983f2e3b
Ali Edalat <ali.edalat.77@gmail.com>
2019-12-09 13:27:50 -0800

    Add CI.

--- src/views/poll/Poll.test.js
+++ src/views/poll/Poll.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Poll';
+import Poll from './Poll';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Poll />, div);
